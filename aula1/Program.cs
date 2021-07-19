519560063a980cb4cc01b09a31641c283ce39cd4
BrunoTruber <brunotruber45@gmail.com>
2021-07-19 20:19:56 -0300

    novo

---
+++ aula1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
